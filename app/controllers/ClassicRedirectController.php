62d86bcf0ea8711d53754e504a5080b6c59dbaca
Nico Verbruggen <nico.verb@gmail.com>
2014-05-13 14:57:02 +0200

    Interpretation of old paths of the classic iRail e.g. http://irail.dev/board/Simonis redirects to the liveboard on http://irail.dev/station/008812013

--- app/controllers/ClassicRedirectController.php
+++ app/controllers/ClassicRedirectController.php
@@ -14,10 +14,29 @@ class ClassicRedirectController extends \BaseController {
      * Redirect classic liveboards URL to the new location
      * and interpret the station name
      */
-    public function redirectBoardSingleStation($station){
-        // TODO: convert station string to station id using Nicola's list of stations
-        // TODO: redirect to the proper url
-        return 'You requested the liveboard for station: ' . $station;
+    public function redirectBoardSingleStation($station_provided_string){
+        $json = file_get_contents('http://' . _DOMAIN_ . '/data/stations.json');
+        $data = json_decode($json);
+        foreach($data->stations as $station){
+            // TODO: write an array with station name alternates
+            // This also solves multilanguage issues since the strings are simply converted
+            // to the proper id :) This way, we can make iRail multilanguage!
+            /*
+             * FOR EXAMPLE:
+             *
+             * $alternates = array(
+             * "Gent-Sint-Pieters" => array('Ghent-Sint-Pieters', 'Gent Sint Pieters', 'Ghent Sint Pieters')
+             * )
+             */
+            // TODO: write a function that loops through station name alternates
+            // If the provided searchstring is contained within an item
+            // Redirect to that station
+            if (strpos($station->name,$station_provided_string) !== false) {
+                header("HTTP/1.1 301 Moved Permanently");
+                header("Location: http://" . _DOMAIN_ . "/station/" . $station->id);
+            }
+        }
+        return 'Liveboard for the following station: ' . $station_provided_string . ' was not found.';
     }
 
     /**
