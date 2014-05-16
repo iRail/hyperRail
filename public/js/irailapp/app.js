0f3100510de7ee635bd002e0c7d2248b5eec2abb
Nico Verbruggen <nico.verb@gmail.com>
2014-05-16 15:03:02 +0200

    Interpreter. If you enter text that doesn't bind to a specific station (pretty unlikely, except for on mobile!), like 'gent', the interpreter will automatically find the station 'Gent-Sint-Pieters':) If this doesn't work, the app will let you know. Note that aliases don't work here yet.

--- public/js/irailapp/app.js
+++ public/js/irailapp/app.js
@@ -4,24 +4,68 @@
 
     irailapp.controller('StationListCtrl', function ($scope, $http, $filter) {
 
+        // Init departure and destination as undefined
+        $scope.departure = undefined;
+        $scope.destination = undefined;
+
+        // Fetch stations via HTTP GET request
         $http.get('data/stations.json').success(function(data) {
             $scope.stations = data;
             console.log($scope.stations);
         });
 
-        $scope.departure = undefined;
-        $scope.destination = undefined;
-
-        $scope.save = function(){
-            $scope.data = {
-                "departure" : $scope.departure,
-                "destination" : $scope.destination,
-                "date" : $filter('date')($scope.mydate, 'shortDate'),
-                "time" : $filter('date')($scope.mytime, 'HH:mm'),
-                "timeoption" : $scope.timeoption
+        // Check if we can dump the data
+        $scope.saveDataCheck = function(){
+            if('id' in $scope.departure && 'id' in $scope.destination){
+                $scope.data = {
+                    "departure" : $scope.departure,
+                    "destination" : $scope.destination,
+                    "date" : $filter('date')($scope.mydate, 'shortDate'),
+                    "time" : $filter('date')($scope.mytime, 'HH:mm'),
+                    "timeoption" : $scope.timeoption
+                }
             }
-        }
+        };
+        
+        $scope.save = function(){
+            // Check if departure and destination are bound
+            try{
+                $scope.saveDataCheck();
+            }
+            // If not bound, try to bind data (1 attempt)
+            catch(ex){
+                    for (var i = 0, len = $scope.stations.stations.length; i < len; i++) {
+                        try{
+                            if (($scope.stations.stations[i].name.toLowerCase()).indexOf($scope.departure.toLowerCase()) != -1){
+                                arr = $scope.stations.stations;
+                                $scope.departure = arr[i];
+                            }
+                        }
+                        catch(ex){
+                            alert('We could not find that station based on your input. Sorry.');
+                        }
+                        try{
+                            if(($scope.stations.stations[i].name.toLowerCase()).indexOf($scope.destination.toLowerCase()) != -1){
+                                arr = $scope.stations.stations;
+                                $scope.destination = arr[i];
+                            }
+                        }
+                        catch(ex){
+                            alert('We could not find that station based on your input. Sorry.');
+                        }
+                    }
+                    try{
+                        $scope.saveDataCheck();
+                    }catch(ex){
+                        alert('We could not find that station based on your input. Sorry.');
+                    }
+                }
+            // Check if time and date are set
+            // Check if select box has been set
+        };
 
     });
+
+
 
 }());
