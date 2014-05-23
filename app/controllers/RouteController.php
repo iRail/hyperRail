ed173d317dc56b697743cf0260fc7a72e3e4fbac
Nico Verbruggen <nico.verb@gmail.com>
2014-05-23 15:53:34 +0200

    Return JSON-LD & regular JSON via content negotiation (improved)

--- app/controllers/RouteController.php
+++ app/controllers/RouteController.php
@@ -1,7 +1,8 @@
 <?php
 
-class RouteController extends \BaseController {
+use ML\JsonLD\JsonLD;
 
+class RouteController extends \BaseController {
     public function index(){
 
         $negotiator = new \Negotiation\FormatNegotiator();
@@ -16,7 +17,28 @@ class RouteController extends \BaseController {
                 return View::make('route.planner');
                 break;
             case "application/ld+json":
-                // TODO: return JSON-LD
+                // TODO: format JSON-LD
+                $from =  Input::get('from'); // required
+                $to = Input::get('to'); // required
+                $time = Input::get('time'); // optional, default to current time if null
+                $date = Input::get('date'); // optional, default to current date if null
+                $timeSel = Input::get('timeSel');
+                $lang = "nl"; // for this version, default to nl station names
+                $fromName = \hyperRail\StationString::convertToString($from)->name;
+                $toName = \hyperRail\StationString::convertToString($to)->name;
+                try{
+                    $json = trim(file_get_contents('http://api.irail.be/connections/?to=' . $toName . '&from=' . $fromName . '&date=' . $date . '&time=' . $time . '&timeSel=' . $timeSel . '&lang=NL&format=json'));
+                    $context = array(
+                        "connection" => "http://vocab.org/transit/terms/route",
+                    );
+                    // Next, encode the context as JSON
+                    $jsonContext = json_encode($context);
+                    $compacted = JsonLD::compact($json, $jsonContext);
+                    echo JsonLD::toString($compacted, true);
+                }
+                catch(ErrorException $ex){
+                    return "We could not retrieve data. Ensure that you have provided all required parameters: to, from, date, time, timeSel.";
+                }
                 break;
             case "application/json":
                 $from =  Input::get('from'); // required
@@ -28,7 +50,7 @@ class RouteController extends \BaseController {
                 $fromName = \hyperRail\StationString::convertToString($from)->name;
                 $toName = \hyperRail\StationString::convertToString($to)->name;
                 try{
-                    $json = file_get_contents('http://api.irail.be/connections/?to=' . $toName . '&from=' . $fromName . '&date=' . $date . '&time=' . $time . '&timeSel=' . $timeSel . '&lang=NL&format=json');
+                    $json = trim(file_get_contents('http://api.irail.be/connections/?to=' . $toName . '&from=' . $fromName . '&date=' . $date . '&time=' . $time . '&timeSel=' . $timeSel . '&lang=NL&format=json'));
                     return $json;
                 }
                 catch(ErrorException $ex){
@@ -39,8 +61,5 @@ class RouteController extends \BaseController {
                 return View::make('route.planner');
                 break;
         }
-
-
     }
-
 }
