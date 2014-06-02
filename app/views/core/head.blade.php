daeb3cc0bec11de1333b159015e1cb59796f453a
Nico Verbruggen <nico.verb@gmail.com>
2014-06-02 13:37:47 +0200

    Favicon + precomposed icon

--- app/views/core/head.blade.php
+++ app/views/core/head.blade.php
@@ -3,6 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <title>iRail.be</title>
+    <link rel="shortcut icon" href="{{ URL::asset('favicon.ico') }}"/>
     <link rel="stylesheet" href="{{ URL::asset('bower_components/bootstrap-sass/lib/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('bower_components/fontawesome/css/font-awesome.min.css') }}"/>
     <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
