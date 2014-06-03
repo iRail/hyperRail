bd078d9d745404a8579f22d4ae4e1f68c35e183a
Nico Verbruggen <nico.verb@gmail.com>
2014-06-03 11:14:35 +0200

    Changed tab order for route planner.

--- app/views/route/planner.blade.php
+++ app/views/route/planner.blade.php
@@ -16,7 +16,7 @@
                     <div class="form-group">
                         <label for="departure">{{Lang::get('client.fromStation')}}</label>
                         <div class="input-group">
-                        <input type="text" ng-model="departure" placeholder="{{Lang::get('client.typeFromStation')}}" typeahead="station as station.name for station in stations.stations | filter:{name:$viewValue} | limitTo:5" typeahead-template-url="customTemplate.html" class="form-control input-lg">
+                        <input type="text" ng-model="departure" placeholder="{{Lang::get('client.typeFromStation')}}" typeahead="station as station.name for station in stations.stations | filter:{name:$viewValue} | limitTo:5" typeahead-template-url="customTemplate.html" class="form-control input-lg" tabindex=1>
                         <a class="input-group-addon" ng-show="departure.id" href="stations/@{{departure.id}}" target="_blank" data-toggle="tooltip" data-placement="left" title="{{Lang::get('client.viewLiveboard')}}"><i class="fa fa-clock-o"></i></a>
                         <span class="input-group-addon" ng-hide="departure.id"></span>
                         </div>
@@ -24,26 +24,26 @@
                     <div class="form-group">
                         <label for="destination">{{Lang::get('client.toStation')}}</label>
                         <div class="input-group">
-                            <input type="text" ng-model="destination" placeholder="{{Lang::get('client.typeToStation')}}" typeahead="station as station.name for station in stations.stations | filter:{name:$viewValue} | limitTo:5" typeahead-template-url="customTemplate.html" class="form-control input-lg">
+                            <input type="text" ng-model="destination" placeholder="{{Lang::get('client.typeToStation')}}" typeahead="station as station.name for station in stations.stations | filter:{name:$viewValue} | limitTo:5" typeahead-template-url="customTemplate.html" class="form-control input-lg" tabindex=2>
                             <a class="input-group-addon btn" ng-show="destination.id" href="stations/@{{destination.id}}" target="_blank" data-toggle="tooltip" data-placement="left" title="{{Lang::get('client.viewLiveboard')}}"><i class="fa fa-clock-o"></i></a>
                             <span class="input-group-addon" ng-hide="destination.id"></span>
                         </div>
                     </div>
                     <label for="destination">{{Lang::get('client.chooseDate')}}</label>
                     <div class="datepicker">
-                        <datepicker ng-class="time" ng-model="mydate" show-weeks="false"></datepicker>
+                        <datepicker ng-class="time" ng-model="mydate" show-weeks="false" tabindex=3></datepicker>
                     </div>
                     <br/>
                 </div>
                 <div class="col-sm-6">
                     <label for="destination">{{Lang::get('client.chooseTime')}}</label>
-                    <select class="form-control input-lg timepicker" ng-model="timeoption">
+                    <select class="form-control input-lg timepicker" ng-model="timeoption" tabindex=4>
                         <option value="depart">{{Lang::get('client.departureAtHour')}}</option>
                         <option value="arrive">{{Lang::get('client.arrivalAtHour')}}</option>
                     </select>
-                    <timepicker ng-model="mytime" ng-change="changed()" minute-step="15" show-meridian="ismeridian"></timepicker>
+                    <timepicker ng-model="mytime" ng-change="changed()" minute-step="15" show-meridian="ismeridian" tabindex=5></timepicker>
                     <br/>
-                    <input type="submit" class="btn btn-default btn-lg btn-primary btn-wide" ng-click="save()" value="{{Lang::get('client.confirmSearch')}}" ng-hide="departure == destination">
+                    <input type="submit" class="btn btn-default btn-lg btn-primary btn-wide" ng-click="save()" value="{{Lang::get('client.confirmSearch')}}" ng-hide="departure == destination" tabindex=6>
                     <div class="alert alert-danger" ng-show="data === null">
                         <p ng-show="stationnotfound === true">{{Lang::get('client.errorCheckInput')}}</p>
                     </div>
