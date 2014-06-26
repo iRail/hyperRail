f3865c5c33b6fdaa806a28b6dfc4d3d139d8e775
Bram Devries <bram_devries@2to1.be>
2014-06-26 16:42:44 +0200

    Modularised Angular app

---
+++ public/app/js/app.js
@@ -0,0 +1,30 @@
+/*--------------------------------------------------------
+ * SET UP AS ANGULAR APP
+ *-------------------------------------------------------*/
+
+var irailapp = angular.module('irailapp',
+    [
+        'ui.bootstrap',
+        'ngAnimate',
+        'irailapp.controllers',
+    ]
+);
+
+angular.module('irailapp.controllers', []);
+
+/**
+ * Helper method to get a URL parameter
+ * @param {[type]} sParam [description]
+ */
+function GetURLParameter(sParam){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+}
---
+++ public/app/js/iRail/Controllers/StationSearchCtrl.js
@@ -0,0 +1,24 @@
+var StationSearchCtrl = function ($scope, $http, $filter, $timeout) {
+    $http.get('../data/stations.json').success(function(data) {
+        $scope.stations = data;
+    });
+
+    $(document).keypress(function(e) {
+        if(e.which == 13){
+            $('#confirm').focus();
+        }
+    });
+
+    $scope.resetplanner = function(){
+        // Should not do anything
+    }
+}
+
+angular.module('irailapp.controllers')
+    .controller('StationSearchCtrl', [
+        '$scope',
+        '$http',
+        '$filter',
+        '$timeout',
+        StationSearchCtrl
+    ]);
