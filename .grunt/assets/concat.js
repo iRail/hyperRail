5c96b39d7f2cad2e56fcc86eef0035e7ee2d558b
Bram Devries <bram_devries@2to1.be>
2014-06-26 16:09:22 +0200

    Use grunt to generate a single javascript file.

--- .grunt/assets/concat.js
+++ .grunt/assets/concat.js
@@ -2,6 +2,13 @@ module.exports = {
 	js: {
 		files: {
 			'<%= paths.compiled.js %>/scripts.js': [
+                '<%= paths.components.jquery %>',
+                '<%= paths.components.angular %>',
+
+                '<%= components %>/angular-animate/angular-animate.min.js',
+                '<%= components %>/bootstrap-sass/dist/js/bootstrap.js',
+                '<%= components %>/angular-bootstrap/ui-bootstrap-tpls.min.js',
+
 				'<%= paths.original.js %>/**/*.js',
 			],
 		},
