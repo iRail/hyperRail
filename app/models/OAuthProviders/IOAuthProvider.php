36a883a714bcc16982ca2a5e7bc81ab7b4066ff6
Brecht Van de Vyvere <br5cht@hotmail.com>
2014-07-02 11:02:18 +0200

    twitter authorization

---
+++ app/models/OAuthProviders/IOAuthProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace hyperRail\app\models\OAuthProviders;
+
+// Interface for OAuth-providers (Facebook, Twitter etc.)
+interface IOAuthProvider{
+
+	public function login();
+	
+}
+
+?>
