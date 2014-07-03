31b60dd8ea19d175ed8abe2439283b2be6b705ad
Brecht Van de Vyvere <br5cht@hotmail.com>
2014-07-03 07:14:56 +0200

    Test migration usertable

---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+class UsersTableSeeder extends Seeder {
+	public function run() {
+		DB::table('users')->delete();
+
+		$users = array(
+			array(
+				'token'		=>	Hash::make('random_token'),
+				'departure'	=>	'https://irail.be/stations/NMBS/008400219/departures/201406060630c6ee1680fdf8b5eb1703dbf41ef84eb0',
+				'name' 		=> 	'Brecht',
+				'email' 	=> 	'br5cht@hotmail.com'
+			)
+		);
+
+		DB::table('users')->insert($users);
+	}
+}
