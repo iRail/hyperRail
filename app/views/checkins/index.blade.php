6c83f2c3fbea0b18aa8ba08321de91061313d49f
michieldewilde <mich.dewild@gmail.com>
2014-07-11 16:04:37 +0200

    Added view checkins

--- app/views/checkins/index.blade.php
+++ app/views/checkins/index.blade.php
@@ -3,11 +3,30 @@
 	@parent
 @stop
 @section('content')
-	<div class="wrapper" ng-app="irailapp" ng-controller="StationLiveboardCtrl">
+	<div class="wrapper">
         <div id="main">
             @include('core.navigation')
             <div class="container">
-
+                @if (count($checkins) == 0)
+					<div class="alert alert-info">
+						<p>You have no existing travel entries</p>
+                	</div>
+                @else
+                	<div class="row results" ng-show="results">
+            			<div class="col-md-12 col-sm-12">
+	                        <p class="h1">Traveldiary</strong></p>
+	                        <div class="list-group">
+	                        	@foreach ($checkins as $checkin)
+	                        		<a class="list-group-item" href="{{$checkin->departure}}">
+		                                <span class="container33">
+											<p>{{$checkin}}</p>
+		                                </span>
+		                            </a>
+	                        	@endforeach
+	                        </div>
+	                    </div>
+	                </div>
+                @endif
             </div>
         </div>
     </div>
