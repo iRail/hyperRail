c1fc975051fe567750fe054d8e46523922614342
Sam Hendrickx <samhendrickx@gmail.com>
2014-07-21 18:18:43 +0200

    Boxes collapse bug

--- app/views/core/head.blade.php
+++ app/views/core/head.blade.php
@@ -4,3 +4,4 @@
 <meta property="og:url" content="https://irail.be" />
 <meta property="og:image" content="{{ URL::asset('apple-touch-icon-precomposed.png') }}" />
 <title>iRail.be</title>
+	{{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js') }}
