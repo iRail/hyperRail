a10dde8acbc95be0c4f4d78cbdde4988a3b66af3
Nico Verbruggen <nico.verb@gmail.com>
2015-02-23 20:18:27 +0100

    Updated copyright footer + links to new Open Knowledge Belgium website + updated references (now that it's no longer called OKFN Belgium)

--- app/views/core/footer.blade.php
+++ app/views/core/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer container">
     <hr/>
-    <p class="small"><a href="{{ URL::to('language') }}">{{Lang::get('client.language')}}</a> | &copy; 2014, OKFN Belgium. <a href="http://hello.iRail.be" target="_blank">{{Lang::get('client.isPartOf')}}</a> <a href="http://okfn.be/" target="_blank">Open Knowledge Foundation Belgium</a>. | <a href="{{ URL::to('contributors') }}">{{Lang::get('contributors.title')}}</a> | <a href="https://github.com/iRail/hyperRail"><i class="fa fa-github"></i></a></p>
+    <p class="small"><a href="{{ URL::to('language') }}">{{Lang::get('client.language')}}</a> | 'iRail' &copy; 2015, Open Knowledge Belgium. <a href="http://hello.iRail.be" target="_blank">{{Lang::get('client.isPartOf')}}</a> <a href="http://www.openknowledge.be/" target="_blank">Open Knowledge Belgium</a>. | <a href="{{ URL::to('contributors') }}">{{Lang::get('contributors.title')}}</a> | <a href="https://github.com/iRail/hyperRail"><i class="fa fa-github"></i></a></p>
 </footer>
 <script>
     var _gaq = [['_setAccount', 'UA-263695-8'], ['_trackPageview']];
