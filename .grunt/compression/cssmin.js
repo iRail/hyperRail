8909feb4517070d08f0cf4f51405220d2c11a343
Tjoosten <Tjoosten3@gmail.com>
2015-08-17 14:10:23 +0200

    add new laravel installation

--- .grunt/compression/cssmin.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	minify: {
-		expand : true,
-		cwd    : '<%= paths.compiled.css %>',
-		src    : '*.css',
-		dest   : '<%= paths.compiled.css %>',
-		ext    : '.min.css'
-	}
-};
--- .grunt/compression/uglify.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	dist: {
-		expand : true,
-		cwd    : '<%= paths.compiled.js %>',
-		src    : ['*.js'],
-		dest   : '<%= paths.compiled.js %>',
-		ext    : '.min.js',
-	}
-};
