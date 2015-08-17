f1c7068a54690cca8498366e0b53e1ea069e4704
Tjoosten <Tjoosten3@gmail.com>
2015-08-17 22:59:07 +0200

    add hyperRail format convertor

---
+++ app/hyperRail/FormatConvertor.php
@@ -0,0 +1,49 @@
+<?php
+namespace hyperRail;
+use hyperRail\Models\LiveboardItem;
+/**
+ * Class iRailFormatConverter
+ * Converts the old JSON to objects.
+ * @package hyperRail
+ */
+class FormatConverter
+{
+    /**
+     * Converts a list of Liveboard items to an array.
+     * @param $json
+     * @param $id
+     * @param string $format
+     */
+    public static function convertLiveboardData($json, $station_id, $format = "jsonld")
+    {
+        $liveboardCollection = array();
+        $initialData = json_decode($json);
+        // TODO: check if json can be decoded (is it an error?)
+        // Set globals
+        // (which data applies to all possible LiveboardItems we are going to create?)
+        $stationName = $initialData->stationinfo->name;
+        // Convert timestamp to date
+        // TODO: count amount of elements, if null, throw error
+        foreach ($initialData->departures->departure as $departure) {
+            $time = $departure->time;
+            $liveboardItem = new LiveboardItem();
+            $date = date('Ymd', $time);
+            $time = date('Hi', $time);
+            $vehicleShort = explode("BE.NMBS.", $departure->vehicle);
+            $liveboardItem->fill($station_id, $date, $time, $vehicleShort[1], $departure->station, $departure->delay, date('c', $departure->time), $departure->platform);
+            array_push($liveboardCollection, $liveboardItem->toArray());
+        }
+        $context = array(
+            "delay" => "http://semweb.mmlab.be/ns/rplod/delay",
+            "platform" => "http://semweb.mmlab.be/ns/rplod/platform",
+            "scheduledDepartureTime" => "http://semweb.mmlab.be/ns/rplod/scheduledDepartureTime",
+            "headsign" => "http://vocab.org/transit/terms/headsign",
+            "routeLabel" => "http://semweb.mmlab.be/ns/rplod/routeLabel",
+            "stop" => array(
+                "@id" => "http://semweb.mmlab.be/ns/rplod/stop",
+                "@type" => "@id"
+            ),
+        );
+        return array("@context" => $context, "@graph" => $liveboardCollection);
+    }
+}
