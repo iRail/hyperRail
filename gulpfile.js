5d22a4c0257553f60d3d80a5f73d827adb2fe883
Tjoosten <Tjoosten3@gmail.com>
2015-08-18 17:05:07 +0200

    buggy implement gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,35 @@
 var elixir = require('laravel-elixir');
+var gulp   = require('gulp');
+var sass   = require('gulp-ruby-sass');
+var bower  = require('gulp-bower');
+var notify = require('gulp-notify');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var config = {
+    sassPath: './resources/assets/sass',
+    bowerDir: './bower_components'
+}
+
+gulp.task('bower', function () {
+    return bower()
+        .pipe(gulp.dest(config.bowerDir))
+});
+
+gulp.task('icons', function () {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+        .pipe(gulp.dest('./public/assets/fonts'));
+});
+
+gulp.task('css', function () {
+    return sass(config.sassPath + '/main.scss', {
+        style: 'compressed',
+        loadPath: [
+            './resources/sass',
+            config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+            config.bowerDir + '/fontawesome/scss',
+        ]
+    })
+        .on("error", notify.onError(function (error) {
+            return "Error: " + error.message;
+        }))
+        .pipe(gulp.dest('./public/assets/css'));
+});
