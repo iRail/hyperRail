6b5668ab016e90bd2329b72ee20cad8a60dace83
Tjoosten <Tjoosten3@gmail.com>
2015-08-22 21:49:56 +0200

    phpunit: removed unused use statements

--- tests/languageTest.php
+++ tests/languageTest.php
@@ -1,9 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
 class languageTest extends TestCase
 {
     public function testLanguage()
