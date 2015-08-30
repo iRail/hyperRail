119f59d2f59a8aaeea7d54047a3ce854e1ab60ae
SaschaDens <sascha.dens@telenet.be>
2015-08-30 10:08:26 +0200

    Refactor url-short to named routes

--- app/Http/Controllers/ClassicRedirectController.php
+++ app/Http/Controllers/ClassicRedirectController.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace app\Http\Controllers;
 
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Http\Request;
-use App\Http\Requests;
 
 class ClassicRedirectController extends Controller
 {
@@ -15,18 +14,18 @@ class ClassicRedirectController extends Controller
     }
 
     /**
-     * Redirect classic liveboards URL to the new location
+     * Redirect classic liveboards URL to the new location.
      */
     public function redirectBoard()
     {
-        return Redirect::to("http://" . Config::get('app.url-short') . "/liveboard", 301);
+        //return Redirect::to("http://" . Config::get('app.url-short') . "/liveboard", 301);
     }
 
     /**
      * Redirect classic liveboards URL to the new location
-     * and interpret the station name
+     * and interpret the station name.
      *
-     * @param  string, $station_provided_string.
+     * @param string, $station_provided_string.
      *
      * @return null|string
      */
@@ -34,20 +33,20 @@ class ClassicRedirectController extends Controller
     {
         $station_converted = \hyperRail\StationString::convertToId($station_provided_string);
         if ($station_converted != null) {
-            header("HTTP/1.1 301 Moved Permanently");
-            header("Location: https://" . Config::get('app.url-short') . "/station/" . $station_converted->id);
+            return Redirect::route('station.index', ['id' => $station_converted->id], 301);
         } else {
-            return 'Liveboard for the following station: ' . $station_provided_string . ' was not found.';
+            return 'Liveboard for the following station: '.$station_provided_string.' was not found.';
         }
-        return null;
+
+        return;
     }
 
     /**
      * Redirect classic liveboards URL to the new location
-     * and interpret two station names
+     * and interpret two station names.
      *
-     * @param  string $station  The first station.
-     * @param  string $station2 The second station.
+     * @param string $station  The first station.
+     * @param string $station2 The second station.
      *
      * @return string
      */
@@ -58,7 +57,7 @@ class ClassicRedirectController extends Controller
 
     /**
      * Redirect classic routing from the old iRail to the new
-     * way the routing is done
+     * way the routing is done.
      *
      * @param $departure_station,   the departure station.
      * @param $destination_station, the destination station.
@@ -70,15 +69,18 @@ class ClassicRedirectController extends Controller
         $departure = \hyperRail\StationString::convertToId($departure_station);
         $destination = \hyperRail\StationString::convertToId($destination_station);
         if ($departure != null && $destination != null) {
-            header("HTTP/1.1 301 Moved Permanently");
-            return Redirect::to("https://" . Config::get('app.url-short') . "/route" .
-                "?from=" . $departure->{'@id'} . "&to=" . $destination->{'@id'} . "&time="
-                . date("Hi") . "&auto=true");
+            // header("HTTP/1.1 301 Moved Permanently");
+            //return Redirect::to("https://" . Config::get('app.url-short') . "/route" .
+            //  "?from=" . $departure->{'@id'} . "&to=" . $destination->{'@id'} . "&time="
+            //  . date("Hi") . "&auto=true");
+
+            // return to Main route as $departure->{'@id'} returns full URL
+            return Redirect::action('RouteController@index', 301);
         } else {
             return "It looks like we couldn't convert your route request to the new format :(";
         }
     }
-    
+
     public function redirectSettings()
     {
         return 'iRail has been changed. iRail no longer has a dedicated settings page.';
