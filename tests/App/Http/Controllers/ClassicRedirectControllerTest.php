97198605175b18466e1dd0d7743694914f5f92d4
Tjoosten <Tjoosten3@gmail.com>
2015-09-14 13:50:43 +0200

    add unit test for redirectSettings

---
+++ tests/App/Http/Controllers/ClassicRedirectControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\ClassicRedirectController;
+
+class ClassicRedirectControllerTest extends TestCase
+{
+    public function testRedirectSettings()
+    {
+        $controller = new ClassicRedirectController();
+        $data = $controller->redirectSettings();
+        $expected = 'iRail has been changed. iRail no longer has a dedicated settings page.';
+
+        $this->assertEquals($data, $expected);
+
+    }
+}
