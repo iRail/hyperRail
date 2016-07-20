2ca94e64a07b5d83e2e1832d06ba6d884bbe96cf
Xavier Bertels <github@xavierbertels.com>
2016-07-20 11:03:26 +0200

    Better balanced elements on home

--- resources/views/_partials/route/planner.blade.php
+++ resources/views/_partials/route/planner.blade.php
@@ -36,45 +36,57 @@
 
     </div>
 
-    <div class="panel-group" id="accordion">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <a data-toggle="collapse" data-parent="#accordion" href="#collapseCalendar" class="btn btn-ghost btn-lg btn-block">
-                    {{-- {!! Lang::get('client.chooseDate')!!} --}}
-                    <span ng-show="timeoption == 'depart'">{!! Lang::get('client.departureAtHour')!!}</span>
-                    <span ng-show="timeoption == 'arrive'">{!! Lang::get('client.arrivalAtHour')!!}</span>
-                    &nbsp;
-                    <i class="fa fa-clock-o"></i>&nbsp;@{{mytime | date:'HH:mm' }}
-                    &nbsp;
-                    <i class="fa fa-calendar"></i>&nbsp;@{{mydate | date:'yyyy–MM–dd' }}
-                </a>
-            </div>
-            <div id="collapseCalendar" class="panel-collapse collapse">
-                <div class="panel-body">
-                    <label for="departureTime" class="sr-only">{!! Lang::get('client.chooseTime')!!}</label>
+    <div class="row">
+        <div class="col-sm-9">
+            <div class="panel-group" id="accordion">
+                <div class="panel panel-default">
+                    <div class="panel-heading p0">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseCalendar" class="btn btn-ghost btn-lg btn-block">
+                            {{-- {!! Lang::get('client.chooseDate')!!} --}}
+                            <span ng-show="timeoption == 'depart'">{!! Lang::get('client.departureAtHour')!!}</span>
+                            <span ng-show="timeoption == 'arrive'">{!! Lang::get('client.arrivalAtHour')!!}</span>
+                            &nbsp;
+                            <i class="fa fa-clock-o"></i>&nbsp;@{{mytime | date:'HH:mm' }}
+                            &nbsp;
+                            <i class="fa fa-calendar"></i>&nbsp;@{{mydate | date:'yyyy–MM–dd' }}
+                        </a>
+                    </div>
+                    <div id="collapseCalendar" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            <label for="departureTime" class="sr-only">{!! Lang::get('client.chooseTime')!!}</label>
 
-                    <label class="radio-inline"><input type="radio" name="departureTime" ng-model="timeoption" value="depart">{!! Lang::get('client.departureAtHour')!!}</label>
-                    <label class="radio-inline"><input type="radio" name="departureTime" ng-model="timeoption" value="arrive">{!! Lang::get('client.arrivalAtHour')!!}</label>
+                            <label class="radio-inline"><input type="radio" name="departureTime" ng-model="timeoption" value="depart">{!! Lang::get('client.departureAtHour')!!}</label>
+                            <label class="radio-inline"><input type="radio" name="departureTime" ng-model="timeoption" value="arrive">{!! Lang::get('client.arrivalAtHour')!!}</label>
 
-                    <timepicker ng-model="mytime" ng-change="changed()" minute-step="15" show-meridian="ismeridian"></timepicker>
-                    <div class="datepicker">
-                        <datepicker ng-class="time" ng-model="mydate" show-weeks="false" starting-day="1"></datepicker>
+                            <timepicker ng-model="mytime" ng-change="changed()" minute-step="15" show-meridian="ismeridian"></timepicker>
+                            <div class="datepicker">
+                                <datepicker ng-class="time" ng-model="mydate" show-weeks="false" starting-day="1"></datepicker>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="col-sm-3">
+            <button type="submit" id="confirm" class="btn btn-default btn-lg btn-primary btn-block" ng-click="save()">
+                {!! Lang::get('client.confirmSearch')!!}
+                <i class="fa fa-angle-right"></i>
+            </button>
+
+            {{-- ng-hide="departure == destination" --}}
+        </div>
     </div>
 
-    <br/>
-
-    <input type="submit" id="confirm" class="btn btn-default btn-lg btn-primary btn-wide" ng-click="save()" value="{!! Lang::get('client.confirmSearch')!!}" ng-hide="departure == destination">
-
     <div class="alert alert-danger" ng-show="data === null">
-        <p ng-show="stationnotfound === true">{!! Lang::get('client.errorCheckInput')!!}</p>
+        <p ng-show="stationnotfound === true">
+            {!! Lang::get('client.errorCheckInput')!!}
+        </p>
     </div>
 
     <div class="alert alert-info" ng-show="departure == destination && departure != null && destination != null">
-        <p>{!!  Lang::get('client.stationsIdentical')!!}</p>
+        <p>
+            {!!  Lang::get('client.stationsIdentical')!!}
+        </p>
     </div>
 
 </div>
