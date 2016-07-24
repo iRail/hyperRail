f5ff2a5b9de77eb10751dffc066f99f7670e01ec
Xavier Bertels <github@xavierbertels.com>
2016-07-24 09:58:53 +0200

    Remove PT Sans font, faster loadtime

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -3,8 +3,6 @@
     <head>
         <title>Be right back.</title>
 
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
         <style>
             html, body {
                 height: 100%;
