3575063f879a705ec71292c16ad8d09fdc4c66ef
Pieter Colpaert <pieter.colpaert@okfn.org>
2016-09-18 13:24:15 +0200

    Removed copyright: is implicit anyways

--- resources/views/core/footer.blade.php
+++ resources/views/core/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer container">
     <hr/>
-    <p class="small text-muted text-center"><a href="{{ URL::to('language') }}">{{Lang::get('client.language')}}</a> | 'iRail' &copy; {{ date('Y') }}, Open Knowledge Belgium. <a href="http://hello.iRail.be" target="_blank"></a>{{Lang::get('client.isPartOf')}} <a href="http://www.openknowledge.be/" target="_blank">Open Knowledge Belgium</a>. | <a href="{{ URL::to('contributors') }}">{{Lang::get('contributors.title')}}</a> | <a href="https://github.com/iRail/hyperRail"><i class="fa fa-github"></i></a></p>
+    <p class="small text-muted text-center"><a href="{{ URL::to('language') }}">{{Lang::get('client.language')}}</a> | 'iRail' - Open Knowledge Belgium. <a href="http://hello.iRail.be" target="_blank"></a>{{Lang::get('client.isPartOf')}} <a href="http://www.openknowledge.be/" target="_blank">Open Knowledge Belgium</a>. | <a href="{{ URL::to('contributors') }}">{{Lang::get('contributors.title')}}</a> | <a href="https://github.com/iRail/hyperRail"><i class="fa fa-github"></i></a></p>
 
 </footer>
 <script>
