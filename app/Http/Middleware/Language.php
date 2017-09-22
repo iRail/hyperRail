6daa0a9a71fe3cd5b6407a5269d28e5f0aeb8565
Bertware <bert.marcelis@outlook.com>
2017-09-22 16:22:49 +0200

    Applied StyleCI patch #262

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -3,10 +3,10 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Session;
 
 class Language
 {
