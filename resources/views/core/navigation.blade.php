b2bdb18280a0bf1b48affe7d63ce156eed00f0c9
Bertware <bert.marcelis@outlook.com>
2017-10-05 16:56:53 +0200

    Open links to external sites in a new tab/window

--- resources/views/core/navigation.blade.php
+++ resources/views/core/navigation.blade.php
@@ -17,8 +17,8 @@
                 <li><a href="{{ URL::to('route') }}" ng-click="resetplanner($event)"><i class="fa fa-road fa-12"></i> {{Lang::get('client.planNewRoute')}}</a></li>
                 <li><a href="{{ URL::to('stations/NMBS') }}"><i class="fa fa-search fa-12"></i> {{Lang::get('client.searchStations')}}</a></li>
                 <li><a href="{{ URL::to('spitsgids') }}">Spitsgids</a></li>
-                <li><a href="https://docs.irail.be">{{Lang::get('client.apidocs')}}</a></li>
-                <li><a href="https://hello.irail.be">Blog</a></li>
+                <li><a href="https://docs.irail.be" target="_blank">{{Lang::get('client.apidocs')}}</a></li>
+                <li><a href="https://hello.irail.be" target="_blank">Blog</a></li>
             </ul>
         </div><!--/.nav-collapse -->
     </div>
