6ba99e92dbb0f9f109ec88eee5d09c0bc67800ed
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-12-07 08:50:53 +0100

    Typo: Add missing space.

--- resources/views/stations/departureConnection.blade.php
+++ resources/views/stations/departureConnection.blade.php
@@ -11,7 +11,7 @@
     <meta name="twitter:domain" content="https://iRail.be">
     <meta name="twitter:description"
           content="Train to {{$direction}} departing at {{$departureStation->name}} leaves today at platform {{$stop['platform']}} at {{date('H:i', strtotime($stop['scheduledDepartureTime']))}}<?php if ($stop['delay'] > 0) {
-              echo "with a delay of " . ($stop['delay'] / 60) . ' minutes';
+              echo " with a delay of " . ($stop['delay'] / 60) . ' minutes';
           }?>.">
     <meta name="twitter:image" content="{{ URL::asset('images/train.jpg') }}">
     <meta property="og:title" content="iRail.be"/>
