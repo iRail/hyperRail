74b8cb7b40d4e869e240120bf0eb08d33f0ae132
Michiel Leyman <michiel.leyman@gmail.com>
2018-12-14 17:16:07 +0100

    Create BluebikeController

---
+++ app/Http/Controllers/BluebikeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+
+class BluebikeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('language');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Response::make($this::getJSON())
+            ->header('Content-Type', 'application/json')
+            ->header('Cache-Control', 'max-age=600');
+    }
+
+    public static function getJSON()
+    {
+        return [ 'some' => 'bikes '];
+    }
+}
